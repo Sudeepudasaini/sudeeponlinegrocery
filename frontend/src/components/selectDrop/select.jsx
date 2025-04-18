0587e7f66715ac2e497c4384a76484c208c3d746
Sudeeppudasaini <heroclan82@gmail.com>
2025-04-18 21:07:00 +0545

    little update in dropdown

--- frontend/src/components/selectDrop/select.jsx
+++ frontend/src/components/selectDrop/select.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-
+import keyboardArrowDown from '@mui/icons-material/keyboardArrowDown';
 const Select =()=>{
 
 const [isOpenSelect,setisOpenSelect]=useState(false);
@@ -9,14 +9,14 @@ const openSelect=()=>{
     setisOpenSelect(!isOpenSelect);
 }
 const closeSelect=(index)=>{
-    setisOpenSelect(!isOpenSelect);
-}
-
+    setselectedIndex(index);
+    setisOpenSelect(false);
+};
     return(
 
 
         <div className="relative w-[40%] text-[16px] font-medium cursor-pointer after:content-[''] after:absolute after:top-[5px] after:right-0 after:w-[1px] after:h-[30px] after:bg-[rgba(0,0,0,0.2)]">
-  <span onClick={openSelect}>All Categories</span>
+  <span onClick={openSelect}>All Categories <keyboardArrowDownIcon className="absolute top-[8px] right-[5px] text-[25px]"/></span>
 {
     isOpenSelect===true &&
 
@@ -28,50 +28,127 @@ const closeSelect=(index)=>{
             className="w-full h-[50px] border border-[#bce3c9] outline-none p-[0px_15px]"
           />
           <ul className="searchResults w-full mt-[10px] p-[10px_0px] max-h-[300px] overflow-y-scroll">
-            <li onClick={()=>closeSelect(0)}>All Categories</li>
-            <li  onClick={()=>closeSelect(1)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Milks and Dairies
-            </li>
-            <li onClick={()=>closeSelect(2)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Wines & Drinks
-            </li>
-            <li onClick={()=>closeSelect(3)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Clothing & Beauty
-            </li>
-            <li onClick={()=>closeSelect(4)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Fresh Seafood
-            </li>
-            <li onClick={()=>closeSelect(5)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Pet Foods & Toy
-            </li>
-            <li onClick={()=>closeSelect(6)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Fast Food
-            </li>
-            <li onClick={()=>closeSelect(7)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Baking Material
-            </li>
-            <li onClick={()=>closeSelect(8)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Vegetables
-            </li>
-            <li onClick={()=>closeSelect(9)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Fresh Fruits
-            </li>
-            <li onClick={()=>closeSelect(10)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Bread and Juice
-            </li>
-            <li onClick={()=>closeSelect(11)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Milks and Dairies
-            </li>
-            <li onClick={()=>closeSelect(12)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Wines & Drinks
-            </li>
-            <li onClick={()=>closeSelect(13)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Clothing & Beauty
-            </li>
-            <li onClick={()=>closeSelect(14)} className="list-none text-[16px] w-full p-[10px] hover:bg-[#3bb77e] hover:text-white cursor-pointer">
-              Fresh Seafood
-            </li>
-          </ul>
+          <li onClick={() => closeSelect(0)} className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 0 ? 'bg-[#f1f1f1]' : ''
+    }`}>
+  All Categories
+</li>
+      
+<li
+    onClick={() => closeSelect(1)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 1 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Milks and Dairies
+  </li>
+  <li
+    onClick={() => closeSelect(2)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 2 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Wines & Drinks
+  </li>
+  <li
+    onClick={() => closeSelect(3)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 3 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Clothing & Beauty
+  </li>
+  <li
+    onClick={() => closeSelect(4)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 4 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Fresh Seafood
+  </li>
+  <li
+    onClick={() => closeSelect(5)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 5 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Pet Foods & Toy
+  </li>
+  <li
+    onClick={() => closeSelect(6)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 6 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Fast Food
+  </li>
+  <li
+    onClick={() => closeSelect(7)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 7 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Baking Material
+  </li>
+  <li
+    onClick={() => closeSelect(8)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 8 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Vegetables
+  </li>
+  <li
+    onClick={() => closeSelect(9)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 9 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Fresh Fruits
+  </li>
+  <li
+    onClick={() => closeSelect(10)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 10 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Bread and Juice
+  </li>
+  <li
+    onClick={() => closeSelect(11)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 11 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Milks and Dairies
+  </li>
+  <li
+    onClick={() => closeSelect(12)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 12 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Wines & Drinks
+  </li>
+  <li
+    onClick={() => closeSelect(13)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 13 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Clothing & Beauty
+  </li>
+  <li
+    onClick={() => closeSelect(14)}
+    className={`list-none text-[16px] text-black w-full p-[10px] cursor-pointer hover:bg-[#3bb77e] hover:text-white ${
+      selectedIndex === 14 ? 'bg-[#f1f1f1]' : ''
+    }`}
+  >
+    Fresh Seafood
+  </li>
+</ul>
+
+      
         </div>
       </div>
 }
